ff3a364a1815925255b53659eb363386d0aa5309
Jingwen Owen Ou <jingweno@gmail.com>
2017-12-26 20:38:55 -0800

    Bump homebrew formula

--- Formula/spotctl.rb
+++ Formula/spotctl.rb
@@ -1,9 +1,9 @@
 class Spotctl < Formula
   desc "A CLI to Spotify"
   homepage "https://github.com/jingweno/spotctl"
-  version "1.0.0"
-  sha256 "89e926f5c1e66f466ea5607e30bc6b120c6363ff0df407e497aa0b7af8151127"
-  url "https://github.com/jingweno/spotctl/releases/download/v1.0.0/darwin-amd64-1.0.0.tar.gz"
+  version "1.0.1"
+  sha256 "a0276bd0c0fb65e7b24885f17d3547276dcf9b059b9507abe51edfa5f5388f89"
+  url "https://github.com/jingweno/spotctl/releases/download/v1.0.1/darwin-amd64-1.0.1.tar.gz"
 
   def install
     bin.install "spotctl"
